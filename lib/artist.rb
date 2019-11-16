23608c7cd12839581f1fb2e1975bc177e5ef8f84
kelly r maass <kmaass77627@gmail.com>
2019-11-16 22:54:59 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist 
+  
+  attr_accessor :name, :song 
+  
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def self.songs 
+    @songs
+  end 
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+  end 
+  
+   def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+  
+  def self.song_count 
+    @songs.count
+    end
+  
+  
+end 
+
