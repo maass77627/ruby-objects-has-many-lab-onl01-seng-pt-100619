2f51db8f0fa98fab18c04ed9256b214877cf889d
kelly r maass <kmaass77627@gmail.com>
2019-11-16 23:26:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,8 @@ class Artist
     song.artist = self 
   end 
   
-   def add_song_by_name(name)
-    song = Song.new(name)
+   def add_song_by_name(song_name)
+    song = Song.new(song_name)
     add_song(song)
   end
   
